3282c0b22c766d4f3074979eec044b25ad767d52
Emre Ovet <eovet@dons.usfca.edu>
2018-10-01 07:14:09 +0000

    fix error in script

--- fastq_sequence_processing.sh
+++ fastq_sequence_processing.sh
@@ -13,7 +13,7 @@ echo "Dowloading files to raw_data"
 
 for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
 do
-    fastq-dump -v $SRA_number -O data/raw_data
+    fastq-dump -v "$SRA_number" -O data/raw_data
 done
 
 
@@ -30,7 +30,7 @@ echo "Trimming the sequences based on their quality scores"
 
 for trim in data/raw_data/*.fastq
 do
-    TrimmomaticSE -threads 2 -phred33 $trim data/trimmed/$(basename "$trim" .fastq).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
+    TrimmomaticSE -threads 2 -phred33 "$trim" data/trimmed/"$(basename "$trim" .fastq).trim.fastq" LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
 done
 
 
@@ -40,7 +40,7 @@ echo "Converting fastq files into fasta files"
 
 for convert in data/trimmed/*.trim.fastq
 do
-    bioawk -c fastx '{print ">"$name"\n"$seq}' $convert > data/trimmed/$(basename -s .fastq $convert).fasta
+    bioawk -c fastx '{print ">"$name"\n"$seq}' "$convert" > data/trimmed/'$(basename -s .fastq $convert).fasta'
 done
 
 
@@ -50,5 +50,5 @@ echo "Using blastn to search for the match of each sequence against the nt datab
 
 for blast in data/trimmed/*.fasta
 do
-    blastn -db /blast-db/nt -num_threads 2 -outfmt '10 sscinames std' -out output/"$(basename -s .trim.fasta "$blast")"_blast_results.csv -max_target_seqs 1 -negative_gilist /blast-db/2018-09-19_environmental_sequence.gi -query $blast
+    blastn -db /blast-db/nt -num_threads 2 -outfmt '10 sscinames std' -out output/"$(basename -s .trim.fasta "$blast")"_blast_results.csv -max_target_seqs 1 -negative_gilist /blast-db/2018-09-19_environmental_sequence.gi -query "$blast"
 done
